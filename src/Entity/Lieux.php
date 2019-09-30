e321f2ed05a99ec15e33f6cc64ff725db45063db
rdanglos2018 <robyn.danglos@gmail.com>
2019-09-30 15:03:29 +0200

    Generation class from bdd

---
+++ src/Entity/Lieux.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lieux
+ *
+ * @ORM\Table(name="lieux", indexes={@ORM\Index(name="lieux_villes_fk", columns={"villes_no_ville"})})
+ * @ORM\Entity
+ */
+class Lieux
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="no_lieu", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $noLieu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_lieu", type="string", length=30, nullable=false)
+     */
+    private $nomLieu;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="rue", type="string", length=30, nullable=true)
+     */
+    private $rue;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="latitude", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $latitude;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="longitude", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $longitude;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="villes_no_ville", type="integer", nullable=false)
+     */
+    private $villesNoVille;
+
+    public function getNoLieu()
+    {
+        return $this->noLieu;
+    }
+
+    public function getNomLieu()
+    {
+        return $this->nomLieu;
+    }
+
+    public function setNomLieu(string $nomLieu)
+    {
+        $this->nomLieu = $nomLieu;
+
+        return $this;
+    }
+
+    public function getRue()
+    {
+        return $this->rue;
+    }
+
+    public function setRue(string $rue)
+    {
+        $this->rue = $rue;
+
+        return $this;
+    }
+
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getVillesNoVille()
+    {
+        return $this->villesNoVille;
+    }
+
+    public function setVillesNoVille(int $villesNoVille)
+    {
+        $this->villesNoVille = $villesNoVille;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/Participants.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Participants
+ *
+ * @ORM\Table(name="participants", uniqueConstraints={@ORM\UniqueConstraint(name="participants_pseudo_uk", columns={"pseudo"})})
+ * @ORM\Entity
+ */
+class Participants
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="no_participant", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $noParticipant;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pseudo", type="string", length=30, nullable=false)
+     */
+    private $pseudo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=30, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=30, nullable=false)
+     */
+    private $prenom;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="telephone", type="string", length=15, nullable=true)
+     */
+    private $telephone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=20, nullable=false)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mot_de_passe", type="string", length=20, nullable=false)
+     */
+    private $motDePasse;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="administrateur", type="boolean", nullable=false)
+     */
+    private $administrateur;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="actif", type="boolean", nullable=false)
+     */
+    private $actif;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="sites_no_site", type="integer", nullable=false)
+     */
+    private $sitesNoSite;
+
+    public function getNoParticipant()
+    {
+        return $this->noParticipant;
+    }
+
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo)
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone)
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getMotDePasse()
+    {
+        return $this->motDePasse;
+    }
+
+    public function setMotDePasse(string $motDePasse)
+    {
+        $this->motDePasse = $motDePasse;
+
+        return $this;
+    }
+
+    public function getAdministrateur()
+    {
+        return $this->administrateur;
+    }
+
+    public function setAdministrateur(bool $administrateur)
+    {
+        $this->administrateur = $administrateur;
+
+        return $this;
+    }
+
+    public function getActif()
+    {
+        return $this->actif;
+    }
+
+    public function setActif(bool $actif)
+    {
+        $this->actif = $actif;
+
+        return $this;
+    }
+
+    public function getSitesNoSite()
+    {
+        return $this->sitesNoSite;
+    }
+
+    public function setSitesNoSite(int $sitesNoSite)
+    {
+        $this->sitesNoSite = $sitesNoSite;
+
+        return $this;
+    }
+
+
+}
