42ed077a42315011cb16e5048f322817c25988a7
jrabenarison2018 <jrabenarison@gmail.com>
2019-09-30 16:41:13 +0200

    ajout fichier d'affichage liste sortie maj SortieController (ajout use + nvelle fonction)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,8 +3,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Sorties;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class SortieController
+
+class SortieController extends Controller
 {
+
+    /**
+     * @Route("/sortie/liste", name="liste_sortie")
+     */
+    public function afficherListe(){}
 
 }
