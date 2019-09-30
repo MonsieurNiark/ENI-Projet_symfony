73205357e61e45ea5f4590228fc3792e11e66a24
rdanglos2018 <robyn.danglos@gmail.com>
2019-09-30 16:46:51 +0200

    Controller Participants

---
+++ src/Controller/ParticipantsController.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Entity\Participants;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class ParticipantsController extends Controller{
+
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request,
+                             UserPasswordEncoderInterface $passwordEncoder,
+                             EntityManagerInterface $em)
+    {
+        $participant = new Participants();
+        $form = $this->createForm(RegistrationType::class, $participant);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $passwordEncoder->encodePassword($participant, $participant->getPassword());
+            $participant->setPassword($password);
+            $participant->setRoles(['ROLE_USER']);
+            $em->persist($participant);
+            $em->flush();
+
+            $this->addFlash("success", "The account has been created!");
+            return $this->redirectToRoute("login");
+        }
+
+        return $this->render("user/register.html.twig", ["form" => $form->createView()]);
+    }
+
+
+}
