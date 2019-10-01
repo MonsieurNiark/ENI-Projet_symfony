b6503676e88d0daa60ed02bd4b2a6d4ac8c6d71a
jrabenarison2018 <jrabenarison@gmail.com>
2019-10-01 09:21:15 +0200

    maj SortieController : fonction afficherListe()

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 
 use App\Entity\Sorties;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class SortieController extends Controller
@@ -13,6 +16,13 @@ class SortieController extends Controller
     /**
      * @Route("/sortie/liste", name="liste_sortie")
      */
-    public function afficherListe(){}
+    public function afficherListe(EntityManagerInterface $em){
+        $sorties = $em->getRepository(Sorties::class);
+
+        return $this->render("Sortie/afficher_liste.html.twig",
+            [
+                "sorties" => $sorties
+            ]);
+    }
 
 }
