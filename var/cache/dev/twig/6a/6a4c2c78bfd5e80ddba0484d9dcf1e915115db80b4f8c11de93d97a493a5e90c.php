f98e8adb36e5dc284e09f77ecdc5ed7a79572f47
ameuniez2018 <a.meuniez@gmail.com>
2019-10-03 10:03:08 +0200

    Footer centré et mieux foutu

--- var/cache/dev/twig/6a/6a4c2c78bfd5e80ddba0484d9dcf1e915115db80b4f8c11de93d97a493a5e90c.php
+++ var/cache/dev/twig/6a/6a4c2c78bfd5e80ddba0484d9dcf1e915115db80b4f8c11de93d97a493a5e90c.php
@@ -46,90 +46,113 @@ class __TwigTemplate_9e8fd45ca6f01b8f77b696105bafdc0a6bd815ead2f7a17c7236036e66b
         // line 1
         echo "<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset=\"UTF-8\">
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-        <title>";
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>";
         // line 6
         $this->displayBlock('title', $context, $blocks);
         echo "</title>
-        <link href=\"";
+    <link href=\"";
         // line 7
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/bootstrap.min.css"), "html", null, true);
         echo "\" rel=\"stylesheet\">
-        <link rel=\"stylesheet\" href=\"";
+    <link rel=\"stylesheet\" href=\"";
         // line 8
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/app.css"), "html", null, true);
         echo "\">
-        ";
+    ";
         // line 9
         $this->displayBlock('stylesheets', $context, $blocks);
         // line 10
-        echo "    </head>
-    <body>
-    <header>
-        <nav class=\"navbar navbar-expand-md navbar-light bg-faded\">
-            <a class=\"nav-link\" href=\"";
+        echo "</head>
+<body>
+<header>
+    <nav class=\"navbar navbar-expand-md navbar-light bg-faded\">
+        <a class=\"nav-link\" href=\"";
         // line 14
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("login");
         echo "\" title=\"Sortir | Home\">
-                <img src=\"";
+            <img src=\"";
         // line 15
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("img/logo.jpg"), "html", null, true);
         echo "\" alt=\"Bucket-List\"> Sortir
-            </a>
-            <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+        </a>
+        <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+
+            <ul class=\"navbar-nav\">
                 ";
-        // line 18
-        if ($this->extensions['Symfony\Bridge\Twig\Extension\SecurityExtension']->isGranted("ROLE_USER")) {
-            // line 19
-            echo "                <ul class=\"navbar-nav\">
-                    <li class=\"nav-item dropdown\">
+        // line 20
+        if ($this->extensions['Symfony\Bridge\Twig\Extension\SecurityExtension']->isGranted("ROLE_ADMIN")) {
+            // line 21
+            echo "                    <li class=\"nav-item dropdown\">
                         <a class=\"nav-link\" href=\"\">Ville</a>
                     </li>
                     <li class=\"nav-item\">
-                        <a class=\"nav-link\" href=\"\">Sites</a>
+                        <a class=\"nav-link\" href=\"";
+            // line 25
+            echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("sites_list");
+            echo "\">Sites</a>
                     </li>
-                    <li class=\"nav-item dropdown\">
-                        <a class=\"nav-link\" href=\"\">Accueil</a>
-                    </li>
-                    <li class=\"nav-item\">
-                        <a class=\"nav-link\" href=\"\">Mon profil</a>
-                    </li>
-                    <li class=\"nav-item\">
-                        <a class=\"nav-link\" href=\"\">Se déconnecter</a>
-                    </li>
-                </ul>
                 ";
         }
-        // line 37
-        echo "            </div>
-        </nav>
-    </header>
-        <div class=\"container\">
+        // line 28
+        echo "                ";
+        if (($this->extensions['Symfony\Bridge\Twig\Extension\SecurityExtension']->isGranted("ROLE_USER") || $this->extensions['Symfony\Bridge\Twig\Extension\SecurityExtension']->isGranted("ROLE_ADMIN"))) {
+            // line 29
+            echo "                <li class=\"nav-item dropdown\">
+                    <a class=\"nav-link\" href=\"";
+            // line 30
+            echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("liste_sortie");
+            echo "\">Accueil</a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"";
+            // line 33
+            echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("my_account");
+            echo "\">Mon profil</a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"";
+            // line 36
+            echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_logout");
+            echo "\">Se déconnecter</a>
+                </li>
+            </ul>
+            ";
+        }
+        // line 40
+        echo "        </div>
+    </nav>
+</header>
+<div class=\"container\">
 
-                ";
-        // line 42
+    ";
+        // line 45
         $this->displayBlock('body', $context, $blocks);
-        // line 43
-        echo "        <footer>
-            © ";
-        // line 44
-        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, "now", "Y"), "html", null, true);
-        echo " Sortir.com
-        </footer>
-        ";
         // line 46
-        $this->displayBlock('javascripts', $context, $blocks);
-        // line 47
-        echo "        <script type=\"text/javascript\" src=\"";
+        echo "
+
+    <script type=\"text/javascript\" src=\"";
+        // line 48
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("js/bootstrap.min.js"), "html", null, true);
         echo "\"></script>
-        <script type=\"text/javascript\" src=\"";
-        // line 48
+    <script type=\"text/javascript\" src=\"";
+        // line 49
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("js/jquery-3.4.1.min.js"), "html", null, true);
         echo "\"></script>
-    </body>
+    ";
+        // line 50
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 51
+        echo "</div>
+<footer>
+    © ";
+        // line 53
+        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, "now", "Y"), "html", null, true);
+        echo " Sortir.com
+</footer>
+</body>
 </html>
 ";
         
@@ -177,7 +200,7 @@ class __TwigTemplate_9e8fd45ca6f01b8f77b696105bafdc0a6bd815ead2f7a17c7236036e66b
 
     }
 
-    // line 42
+    // line 45
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -195,7 +218,7 @@ class __TwigTemplate_9e8fd45ca6f01b8f77b696105bafdc0a6bd815ead2f7a17c7236036e66b
 
     }
 
-    // line 46
+    // line 50
     public function block_javascripts($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -225,60 +248,66 @@ class __TwigTemplate_9e8fd45ca6f01b8f77b696105bafdc0a6bd815ead2f7a17c7236036e66b
 
     public function getDebugInfo()
     {
-        return array (  199 => 46,  181 => 42,  163 => 9,  144 => 6,  130 => 48,  125 => 47,  123 => 46,  118 => 44,  115 => 43,  113 => 42,  106 => 37,  86 => 19,  84 => 18,  78 => 15,  74 => 14,  68 => 10,  66 => 9,  62 => 8,  58 => 7,  54 => 6,  47 => 1,);
+        return array (  222 => 50,  204 => 45,  186 => 9,  167 => 6,  152 => 53,  148 => 51,  146 => 50,  142 => 49,  138 => 48,  134 => 46,  132 => 45,  125 => 40,  118 => 36,  112 => 33,  106 => 30,  103 => 29,  100 => 28,  94 => 25,  88 => 21,  86 => 20,  78 => 15,  74 => 14,  68 => 10,  66 => 9,  62 => 8,  58 => 7,  54 => 6,  47 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Source("<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset=\"UTF-8\">
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-        <title>{% block title %}Sortir.com{% endblock %}</title>
-        <link href=\"{{ asset('css/bootstrap.min.css') }}\" rel=\"stylesheet\">
-        <link rel=\"stylesheet\" href=\"{{ asset('css/app.css') }}\">
-        {% block stylesheets %}{% endblock %}
-    </head>
-    <body>
-    <header>
-        <nav class=\"navbar navbar-expand-md navbar-light bg-faded\">
-            <a class=\"nav-link\" href=\"{{ path('login') }}\" title=\"Sortir | Home\">
-                <img src=\"{{ asset('img/logo.jpg') }}\" alt=\"Bucket-List\"> Sortir
-            </a>
-            <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
-                {% if is_granted(\"ROLE_USER\") %}
-                <ul class=\"navbar-nav\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>{% block title %}Sortir.com{% endblock %}</title>
+    <link href=\"{{ asset('css/bootstrap.min.css') }}\" rel=\"stylesheet\">
+    <link rel=\"stylesheet\" href=\"{{ asset('css/app.css') }}\">
+    {% block stylesheets %}{% endblock %}
+</head>
+<body>
+<header>
+    <nav class=\"navbar navbar-expand-md navbar-light bg-faded\">
+        <a class=\"nav-link\" href=\"{{ path('login') }}\" title=\"Sortir | Home\">
+            <img src=\"{{ asset('img/logo.jpg') }}\" alt=\"Bucket-List\"> Sortir
+        </a>
+        <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+
+            <ul class=\"navbar-nav\">
+                {% if is_granted(\"ROLE_ADMIN\") %}
                     <li class=\"nav-item dropdown\">
                         <a class=\"nav-link\" href=\"\">Ville</a>
                     </li>
                     <li class=\"nav-item\">
-                        <a class=\"nav-link\" href=\"\">Sites</a>
+                        <a class=\"nav-link\" href=\"{{ path('sites_list') }}\">Sites</a>
                     </li>
-                    <li class=\"nav-item dropdown\">
-                        <a class=\"nav-link\" href=\"\">Accueil</a>
-                    </li>
-                    <li class=\"nav-item\">
-                        <a class=\"nav-link\" href=\"\">Mon profil</a>
-                    </li>
-                    <li class=\"nav-item\">
-                        <a class=\"nav-link\" href=\"\">Se déconnecter</a>
-                    </li>
-                </ul>
                 {% endif %}
-            </div>
-        </nav>
-    </header>
-        <div class=\"container\">
+                {% if (is_granted(\"ROLE_USER\") or is_granted(\"ROLE_ADMIN\")) %}
+                <li class=\"nav-item dropdown\">
+                    <a class=\"nav-link\" href=\"{{ path('liste_sortie') }}\">Accueil</a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"{{ path('my_account') }}\">Mon profil</a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"{{ path('app_logout') }}\">Se déconnecter</a>
+                </li>
+            </ul>
+            {% endif %}
+        </div>
+    </nav>
+</header>
+<div class=\"container\">
 
-                {% block body %}{% endblock %}
-        <footer>
-            © {{ \"now\"|date(\"Y\") }} Sortir.com
-        </footer>
-        {% block javascripts %}{% endblock %}
-        <script type=\"text/javascript\" src=\"{{ asset('js/bootstrap.min.js') }}\"></script>
-        <script type=\"text/javascript\" src=\"{{ asset('js/jquery-3.4.1.min.js') }}\"></script>
-    </body>
+    {% block body %}{% endblock %}
+
+
+    <script type=\"text/javascript\" src=\"{{ asset('js/bootstrap.min.js') }}\"></script>
+    <script type=\"text/javascript\" src=\"{{ asset('js/jquery-3.4.1.min.js') }}\"></script>
+    {% block javascripts %}{% endblock %}
+</div>
+<footer>
+    © {{ \"now\"|date(\"Y\") }} Sortir.com
+</footer>
+</body>
 </html>
 ", "base.html.twig", "C:\\wamp\\www\\ENI-Projet_symfony\\templates\\base.html.twig");
     }
