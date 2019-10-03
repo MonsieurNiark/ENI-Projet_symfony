fa23f656fdd0e096becb49b85d4e8fb51f2c88ec
jrabenarison2018 <jrabenarison@gmail.com>
2019-10-03 17:14:52 +0200

    filtre sur la liste des sorties -> OK pas d'inscription si etat = FERMEE

--- src/Repository/SortiesRepository.php
+++ src/Repository/SortiesRepository.php
@@ -31,47 +31,72 @@ class SortiesRepository extends ServiceEntityRepository
     public function getSortieByFiltre(int $idSite, String $nomSortie, int $estOrga, int $estInscrit, int $estPasInscrit, int $sortiePassees, int $idUtilisateur){
         $queryBuilder = $this->getSortiesVisible();
 
+        /* **************** QUERY BUILDER **************** */
         if($idSite != 0){
             $queryBuilder->innerJoin('sortie.siteSortie','site')
                 ->addSelect('site')
-                ->andWhere('site.no_Site = :id_site')
-                ->setParameter('id_site',$idSite);
+                ->andWhere('site.no_Site = :id_site');
         }
 
         if(!is_null($nomSortie) && !empty($nomSortie)){
-            $queryBuilder->andWhere('sortie.nom LIKE :nom_sortie')
-                ->setParameter('nom_sortie','%'.$nomSortie.'%');
-        }
-
-        if($estOrga == 1){
-            $queryBuilder->andWhere('sortie.organisateurSortie = :id_orga')
-                ->setParameter('id_orga',$idUtilisateur);
+            $queryBuilder->andWhere('sortie.nom LIKE :nom_sortie');
         }
 
         if($estInscrit == 1 && $estPasInscrit == 0){
             $queryBuilderInscrit = $this->getEntityManager()->getRepository(Inscriptions::class)
                 ->createQueryBuilder('insc')
-                ->select('insc.sortieInscription')
-//                ->addSelect('insc.sortieInscription')
-                ->andWhere('insc.participantInscription IN (:id_inscrit)')
-                ->setParameter('id_inscrit',$idUtilisateur);
-
+                ->select('sortie_ins.noSortie')
+                ->innerJoin('insc.sortieInscription','sortie_ins')
+                ->andWhere('insc.sortieInscription = sortie_ins.noSortie')
+                ->andWhere('insc.participantInscription IN (:id_inscrit)');
 
             $queryBuilder->innerJoin('sortie.inscriptionsSortie','inscrit')
                 ->where($queryBuilder->expr()->in('sortie.noSortie',$queryBuilderInscrit->getDQL()));
         }
 
         if($estPasInscrit == 1 && $estInscrit == 0){
-            $queryBuilder->innerJoin('sortie.inscriptionsSortie','non_inscrit')
-                ->addSelect('non_inscrit')
-                ->andWhere('non_inscrit.sortieInscription = sortie.noSortie')
-                ->andWhere('non_inscrit.participantInscription NOT IN (:id_non_inscrit)')
-                ->setParameter('id_non_inscrit',$idUtilisateur);
+            $queryBuilderInscrit = $this->getEntityManager()->getRepository(Inscriptions::class)
+                ->createQueryBuilder('noinsc')
+                ->select('sortie_noins.noSortie')
+                ->innerJoin('noinsc.sortieInscription','sortie_noins')
+                ->andWhere('noinsc.sortieInscription = sortie_noins.noSortie')
+                ->andWhere('noinsc.participantInscription IN (:id_noinscrit)');
+
+            $queryBuilder->innerJoin('sortie.inscriptionsSortie','noinscrit')
+                ->where($queryBuilder->expr()->notIn('sortie.noSortie',$queryBuilderInscrit->getDQL()));
+        }
+
+        if($estOrga == 1){
+            $queryBuilder->andWhere('sortie.organisateurSortie = :id_orga');
         }
 
         if ($sortiePassees == 1){
-            $queryBuilder->andWhere('ADDTIME(sortie.datedebut, SEC_TO_TIME(sortie.duree * 60))  < :today')
-                ->setParameter('today', new \DateTime("now"));
+            $queryBuilder->andWhere('DATE_ADD(sortie.datedebut, sortie.duree, \'minute\') < :today');
+        }
+
+        /* **************** SET PARAMETER **************** */
+        if($idSite != 0){
+            $queryBuilder->setParameter('id_site',$idSite);
+        }
+
+        if(!is_null($nomSortie) && !empty($nomSortie)){
+            $queryBuilder->setParameter('nom_sortie','%'.$nomSortie.'%');
+        }
+
+        if($estOrga == 1){
+            $queryBuilder->setParameter('id_orga',$idUtilisateur);
+        }
+
+        if($estInscrit == 1 && $estPasInscrit == 0){
+            $queryBuilder->setParameter('id_inscrit',$idUtilisateur);
+        }
+
+        if($estPasInscrit == 1 && $estInscrit == 0){
+            $queryBuilder->setParameter('id_noinscrit',$idUtilisateur);
+        }
+
+        if ($sortiePassees == 1){
+            $queryBuilder->setParameter('today', new \DateTime("now"));
         }
 
         return $queryBuilder->getQuery()->getResult();
